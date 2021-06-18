27cdeb57d8d67d2f2c06385ac532b377bc8affaa
김지웅 <kjw4611@gmail.com>
2021-06-18 23:37:12 +0900

    [add] chapter 04 - Entity mapping

--- ch02/src/main/java/jpabook/start/Member.java
+++ ch02/src/main/java/jpabook/start/Member.java
@@ -1,21 +1,39 @@
 package jpabook.start;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
-@Table(name="MEMBER")
+@Table(name="MEMBER",
+uniqueConstraints = {   // 유니크 제약조건
+    @UniqueConstraint(
+            name = "NAME_AGE_UNIQUE",   // 제약조건 이름
+            columnNames = {"NAME", "AGE"}   // 제약 조건 대상
+    )
+})
 public class Member {
 
     @Id
     @Column(name = "ID")
     private String id;
 
-    @Column(name = "NAME")
+    @Column(name = "NAME",
+            nullable = false, length = 10)  // 제약조건 추가
     private String username;
     private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private
+
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob // DB CLOB, BLOB 타입 매핑
+    private String description;
 
     public String getId() {
         return id;
