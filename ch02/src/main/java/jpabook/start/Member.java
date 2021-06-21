60e86e96c32b51ec0a7f9f801502a1d2291f3151
김지웅 <kjw4611@gmail.com>
2021-06-21 16:16:55 +0900

    [add] ch04 - Entity Type Annotation

--- ch02/src/main/java/jpabook/start/Member.java
+++ ch02/src/main/java/jpabook/start/Member.java
@@ -4,6 +4,11 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
+/* Access: JPA가 엔티티 데이터에 접근하는 방식 지정
+* FIELD: 필드에 직접 접근, private여도 접근 가능
+* PROPERTY: 접근자 Getter 사용하는 프로퍼티 접근
+* */
+@Access(AccessType.FIELD)
 @Table(name="MEMBER",
 uniqueConstraints = {   // 유니크 제약조건
     @UniqueConstraint(
@@ -22,18 +27,31 @@ public class Member {
     private String username;
     private Integer age;
 
+    /* STRING(권장): enum의 이름 자체로 매핑함
+    * ORDINAL(default): 각 enum의 순서로 매핑함 0, 1, 2 ... */
     @Enumerated(EnumType.STRING)
-    private
+    private RoleType roleType;
 
-
+    /* 날짜 타입(java.util.Date, Calendar)을 매핑할 때 사용
+    * DATE: 년월일만 있는 DB의 date 타입과 매핑
+    * TIME: 시분초만 있는 DB의 time 타입과 매핑
+    * TIMESTAMP: 년월일시분초가 있는 DB의 timestamp(= datetime) 타입과 매핑
+    *  */
     @Temporal(TemporalType.TIMESTAMP)
     private Date createDate;
 
     @Temporal(TemporalType.TIMESTAMP)
     private Date lastModifiedDate;
 
-    @Lob // DB CLOB, BLOB 타입 매핑
+    /* 필드 타입이 문자면 CLOB, 나머지는 BLOB로 매핑 */
+    @Lob
     private String description;
+
+    /* Transient: 이 필드는 매핑하지 않음
+    * DB에 저장하지 않고 조회하지 않음, 임시로 값을 보관하고 싶을 때 사용
+    *  */
+    @Transient
+    private Integer temp;
 
     public String getId() {
         return id;
